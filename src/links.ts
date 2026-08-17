695e250c4b226c9c0e3972c3747ef1aefa8f5681
blanklavender <rudrapatimahima248@gmail.com>
2026-08-16 17:14:43 -0700

    email corrected, cv link

--- src/links.ts
+++ src/links.ts
@@ -1,8 +1,8 @@
 export const LINKEDIN = 'https://linkedin.com/in/mahima-rudrapati';
 export const GITHUB = 'https://github.com/blanklavender';
-export const EMAIL = 'rudrapatimahima248@gmail.com';
-export const HRVIP_LAB = 'https://repashrvip.wordpress.com/';
+export const EMAIL = 'rmahimaa927@gmail.com';
+export const HRVIP_LAB = 'https://hrvip.ucdavis.edu/';
 export const PAPER = 'https://ieeexplore.ieee.org/document/10919373';
 
 export const CV_URL =
-  'https://drive.google.com/file/d/1vYJ0tsaBFvFXtqgT5e75k_Vxoxu7qsQm/view?usp=drive_link';
+  'https://drive.google.com/file/d/1cNUZ4NET6sbFybznUtfrkUlWcT1wXvcy/view?usp=drive_link';
